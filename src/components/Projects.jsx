4f488e12f5049696b0f6a21e9f082dab4acd6a51
ayman kacemi <aymankacemi01@gmail.com>
2023-10-04 23:25:49 +0100

    Fix major problems in proejcts section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
@@ -8,10 +8,8 @@ const Projects = () => {
       <div className='arrowLeft'></div>
       <div className='arrowRight'></div>
       <div className='projects-wrapper'>
-      
-
         <ProjectsCard
-          title={'This Website'}
+          title={'This Website 1'}
           description={'description about the website'}
           icon={
             <i
@@ -22,7 +20,7 @@ const Projects = () => {
           id={1}
         />
         <ProjectsCard
-          title={'This Website'}
+          title={'This Website 2'}
           description={'description about the website'}
           icon={
             <i
@@ -32,7 +30,37 @@ const Projects = () => {
           }
         />
         <ProjectsCard
-          title={'This Website'}
+          title={'This Website 3'}
+          description={'description about the website'}
+          icon={
+            <i
+              className='fa-brands fa-react'
+              style={{ color: 'white', scale: '3.5', opacity: '0.9' }}
+            />
+          }
+        />
+        <ProjectsCard
+          title={'This Website 4'}
+          description={'description about the website'}
+          icon={
+            <i
+              className='fa-brands fa-react'
+              style={{ color: 'white', scale: '3.5', opacity: '0.9' }}
+            />
+          }
+        />
+        <ProjectsCard
+          title={'This Website 5'}
+          description={'description about the website'}
+          icon={
+            <i
+              className='fa-brands fa-react'
+              style={{ color: 'white', scale: '3.5', opacity: '0.9' }}
+            />
+          }
+        />
+        <ProjectsCard
+          title={'This Website 6'}
           description={'description about the website'}
           icon={
             <i
@@ -42,8 +70,22 @@ const Projects = () => {
           }
         />
       </div>
+      {/* <div className='projects-map' style={{ display: 'flex' }}>
+        <span style={style}></span>
+        <span style={style}></span>
+        <span style={style}></span>
+      </div> */}
     </section>
   );
 };
-
+// const style = {
+//   display: 'block',
+//   margin: '1rem',
+//   minWidth: '15px',
+//   minHeight: '15px',
+//   backgroundColor: '#ff9a34',
+//   border: '1px solid black',
+//   borderRadius: '50px',
+//   opacity: '0.5',
+// };
 export default Projects;
