53956261571e5b7557668b376ae4d90f95829286
KACEMI Ayman <35453162+ayman-kacemi47@users.noreply.github.com>
2025-06-21 23:43:07 +0100

    Update Uses.jsx with the new PC config

--- src/components/Uses.jsx
+++ src/components/Uses.jsx
@@ -6,35 +6,29 @@ const Uses = () => {
   return (
     <section id='uses'>
       <h1 style={{ textAlign: 'center', margin: '48px' }}>
-        {' '}
-        {t('usesSectionTitle')}{' '}
+        {t('usesSectionTitle')}
       </h1>
       <ul>
         <li>
-          <strong> {t('usesLaptop')} </strong> hp elitebook x360 1030 g2
+          <strong>{t('usesLaptop')}</strong> HP EliteBook 840 G6
         </li>
         <li>
-          {' '}
-          <strong>{t('usesOperatingSystem')}</strong> Windows 10 Professionnel
-          64-bit
+          <strong>{t('usesOperatingSystem')}</strong> Windows 11 Professionnel 64-bit
         </li>
         <li>
-          <strong>{t('usesProcessor')}</strong> Intel(R) Core(TM) i5-7200U CPU @
-          2.50GHz (4 CPUs), ~2.7GHz
+          <strong>{t('usesProcessor')}</strong> Intel(R) Core(TM) i5-8365U CPU @ 1.60GHz (8 CPUs), ~1.9GHz
         </li>
         <li>
-          <strong>{t('usesApplications')}</strong> VS Code, Chrome, Postman,
-          Figma, Adobe Photoshop ...
+          <strong>{t('usesMemory')}</strong> 32GB RAM
         </li>
         <li>
-          <strong>{t('usesMemory')}</strong> 8GB RAM
+          <strong>{t('usesCardName')}</strong> Intel(R) UHD Graphics 620
         </li>
         <li>
-          <strong> {t('usesCardName')} </strong> Intel(R) HD Graphics 620
+          <strong>{t('usesApplications')}</strong> IntelliJ IDEA, VS Code, Chrome, Postman, Figma, Adobe Photoshop...
         </li>
         <li>
-          <strong>{t('usesPhone')} </strong> Redmi note 10 pro and Galaxy A2
-          Core
+          <strong>{t('usesPhone')}</strong> Redmi Note 10 Pro and Galaxy A2 Core
         </li>
       </ul>
     </section>
